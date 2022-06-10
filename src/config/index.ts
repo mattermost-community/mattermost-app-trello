cb13143140cb93142483d46af29da9fd615ba914
Fermin Castrejon <desarrollo@ancient.mx>
2022-06-10 14:41:29 -0500

    ci(): added ci

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,15 @@
+require('dotenv').config('../')
+
 export default {
     APP: {
-        PORT: 4002,
-        HOST: 'https://107c-201-160-204-97.ngrok.io'
+        PORT: Number(process.env.PORT) || 4002,
+        HOST: process.env.HOST || 'https://107c-201-160-204-97.ngrok.io'
     },
     TRELLO: {
         URL: 'https://api.trello.com/1/'
     },
     MATTERMOST: {
-        URL: 'http://[::1]:8066',
+        URL: process.env.MATTERMOST_SERVER_URL || 'http://[::1]:8066',
         USE: true
     }
 }
