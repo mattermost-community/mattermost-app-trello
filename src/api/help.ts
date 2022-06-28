780565b42859847d7a198dcc7c22cc89d2e32740
Lizeth García <lizeth.garcia@ancient.mx>
2022-06-28 16:35:44 -0500

    style: show help commands from current configuration step

--- src/api/help.ts
+++ src/api/help.ts
@@ -1,17 +1,20 @@
 import {Request, Response} from 'express';
 import manifest from '../manifest.json';
 import {
+    existsKvOauthToken,
+    existsKvTrelloConfig,
     isUserSystemAdmin, 
     newOKCallResponseWithMarkdown
 } from "../utils";
-import {AppCallRequest, AppCallResponse, ExpandedBotActingUser} from "../types";
+import {AppActingUser, AppCallRequest, AppCallResponse, ExpandedBotActingUser} from "../types";
 import {addBulletSlashCommand, h5, joinLines} from "../utils/markdown";
 import {Commands} from "../constant";
+import { KVStoreClient, KVStoreOptions } from '../clients/kvstore';
 
 export const getHelp = async (request: Request, response: Response) => {
     const helpText = [
         getHeader(),
-        getCommands(request.body)
+        await getCommands(request.body)
     ].join('');
     const callResponse: AppCallResponse = newOKCallResponseWithMarkdown(helpText);
 
@@ -22,35 +25,37 @@ function getHeader(): string {
     return h5(`Mattermost Trello Plugin - Slash Command Help`);
 }
 
-function getCommands(call: AppCallRequest): string {
-    console.log('call', call);
+async function getCommands(call: AppCallRequest): Promise<string> {
+    const homepageUrl: string = manifest.homepage_url;
     const context = call.context as ExpandedBotActingUser;
+    const mattermostUrl: string | undefined = context.mattermost_site_url;
+    const botAccessToken: string | undefined = context.bot_access_token;
+    const actingUser: AppActingUser | undefined = context.acting_user;
+    const actingUserID: string | undefined = actingUser.id;
+    const commands: string[] = [];
+
+    const options: KVStoreOptions = {
+        mattermostUrl: <string>mattermostUrl,
+        accessToken: <string>botAccessToken,
+    };
+    const kvClient = new KVStoreClient(options);
     
-    return isUserSystemAdmin(context.acting_user)
-        ? getAdminCommands()
-        : getUserCommands();
-}
+    commands.push(addBulletSlashCommand(Commands.HELP, `Launch the Jira plugin command line help syntax, check out the [documentation](${homepageUrl}).`));
 
-function getUserCommands(): string {
-    const homepageUrl: string = manifest.homepage_url;
+    if (isUserSystemAdmin(<AppActingUser>actingUser)) {
+        commands.push(addBulletSlashCommand(Commands.CONFIGURE, `Configure Trello workspace`));
+    }
+    if (await existsKvTrelloConfig(kvClient)) {
+        if (await existsKvOauthToken(kvClient, <string>actingUserID)) {
+            commands.push(addBulletSlashCommand(`${Commands.CARD} create`, `Create a new card`));
+            commands.push(addBulletSlashCommand(`${Commands.SUBSCRIPTION} add`, 'Subscribe channel to receive notifications from a Trello board'));
+            commands.push(addBulletSlashCommand(`${Commands.SUBSCRIPTION} list`, 'Show all your current subscriptions'));
+            commands.push(addBulletSlashCommand(`${Commands.SUBSCRIPTION} remove`, 'Stop receiving Trello board\'s notifications'));
+        }
 
-    return `${joinLines(
-        addBulletSlashCommand(Commands.HELP, `Launch the Jira plugin command line help syntax, check out the [documentation](${homepageUrl}).`),
-        addBulletSlashCommand(Commands.CARD, `Create a new card`),
-        addBulletSlashCommand(Commands.SUBSCRIPTION, 'Subscribe channel to receive Trello notifications'),
-        addBulletSlashCommand(Commands.ACCOUNT, 'log in and out with trello'),
-    )}\n`;
-}
+        commands.push(addBulletSlashCommand(`${Commands.ACCOUNT} login`, 'Log to your Trello account'));
+        commands.push(addBulletSlashCommand(`${Commands.ACCOUNT} logout`, 'Log out from your Trello account'));
+    }
 
-function getAdminCommands(): string {
-    const homepageUrl: string = manifest.homepage_url;
-
-    return `${joinLines(
-        addBulletSlashCommand(Commands.HELP, `Launch the Jira plugin command line help syntax, check out the [documentation](${homepageUrl}).`),
-        addBulletSlashCommand(Commands.CONFIGURE, `Configure Trello workspace`),
-        addBulletSlashCommand(Commands.HELP, `Launch the Jira plugin command line help syntax, check out the [documentation](${homepageUrl}).`),
-        addBulletSlashCommand(Commands.CARD, `Create a new card`),
-        addBulletSlashCommand(Commands.SUBSCRIPTION, 'Subscribe channel to receive Trello notifications'),
-        addBulletSlashCommand(Commands.ACCOUNT, 'log in and out with trello'),
-    )}\n`;
+    return `${joinLines(...commands)}`;
 }
