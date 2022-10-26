7e3b76fdba09166495f4d1b0e8c1a79b6697b639
Lizeth García <lizeth.garcia@ancient.mx>
2022-10-26 17:13:07 -0500

    ci(): changed structure on index.ts and config/index.ts (MATT-138)

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,8 @@ require('dotenv').config('../')
 
 export default {
     APP: {
-        PORT: Number(process.env.PORT) || 4002,
-        HOST: process.env.HOST || 'https://ee70-201-160-204-97.ngrok.io'
+        PORT: Number(process.env.PORT) || 4501,
+        HOST: process.env.HOST || ''
     },
     TRELLO: {
         URL: 'https://api.trello.com/1/'
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
-import express, {Express} from 'express';
+import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 import config from './config';
 import apiRoutes from './api';
+import morgan from 'morgan';
 
+const serverless = require('serverless-http');
 const app: Express = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
+app.use(morgan('tiny'))
 app.use('/', apiRoutes);
+app.post('/ping', (req, res) => { res.json({}) })
 
-const port: number = config.APP.PORT;
-app.listen(port, () => console.log('Listening on ' + port));
+// App released via HTTP and docker
+if (config.APP.HOST) {
+    const port: number = config.APP.PORT;
+    app.listen(port, () => console.log('Listening on ' + port));
+}
+// App released via AWS Lambda
+else {
+    module.exports.handler = serverless(app);
+}
 
