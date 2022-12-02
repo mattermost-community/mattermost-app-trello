f7c66ec9506697b2a2ce567a271965b086e648cc
Lizeth García <lizeth.garcia@ancient.mx>
2022-12-02 09:50:50 -0600

    feat: removed unused kvClient methods (MATT-170)

--- src/clients/kvstore.ts
+++ src/clients/kvstore.ts
@@ -45,24 +45,4 @@ export class KVStoreClient {
             },
         }).then((response: AxiosResponse<any>) => response.data);
     }
-
-    public kvGet(key: string): Promise<ConfigStoreProps> {
-        const url = `${this.config.mattermostUrl}/plugins/${AppsPluginName}${Routes.Mattermost.ApiVersionV1}${Routes.Mattermost.PathKV}/${key}`;
-        return axios.get(url, {
-            headers: {
-                Authorization: `BEARER ${this.config.accessToken}`,
-                'content-type': 'application/json; charset=UTF-8',
-            },
-        }).then((response: AxiosResponse<any>) => response.data);
-    }
-
-    public getOauth2User(key: string): Promise<StoredOauthUserToken> {
-        const url = `${this.config.mattermostUrl}/plugins/${AppsPluginName}${Routes.Mattermost.ApiVersionV1}${Routes.Mattermost.PathKV}/${key}`;
-        return axios.get(url, {
-            headers: {
-                Authorization: `BEARER ${this.config.accessToken}`,
-                'content-type': 'application/json; charset=UTF-8',
-            },
-        }).then((response: AxiosResponse<any>) => response.data);
-    }
 }
